2842db99931d69ff8c09e0f66405916955a2753e
myk1118 <jaimekim1118@gmail.com>
2019-10-02 20:52:21 -0700

    Started working on displaying cart items on cart page and guest checkout functionality

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import './cart.scss';
 import { connect } from 'react-redux';
 import { getActiveCart } from '../../actions';
+import Money from '../general/money';
+import { Link } from 'react-router-dom';
 
 class Cart extends Component {
     componentDidMount() {
         this.props.getActiveCart();
     }
     render() {
-        // console.log('Cart Items: ', this.props.cartItems);
+        console.log('Cart Items: ', this.props.cartItems);
+        const { cartItems } = this.props;
+        // if (cartItems == null) return false;
         return (
             <div>
-                <h1 className="center">Cart</h1>
+                {cartItems && cartItems.map(v => (
+                    <div key={v.itemId}>
+                        <img src={v.thumbnail.url} alt="cartItem" className="cartItem" />
+                        <div>{v.name}</div>
+                        <div><Money money={v.each} /></div>
+                        <div>{v.quantity}</div>
+                        <div><Money money={v.total} /></div>
+                    </div>
+                ))}
+                <Link to="/checkout/guest" className="guestCheckoutLink">Checkout as Guest</Link>
             </div>
-        )
+        );
     }
 }
 
